dd3560b1d4ff398c9e080c3e2a7f84b2f18ebc2e
Vikram Chandran <vikramchandran@Vikrams-MacBook-Air.local>
2024-04-28 17:01:40 -0700

    Removed duplicate folder

--- vikram-clara/constants/constants.py
+++
@@ -1,8 +0,0 @@
-FILE_NOT_FOUND = "Please restart your Slack app. User's availability data not found"
-USER_NOT_EXIST = "User does not exist"
-USER_DUPLICATE = "Please provide email because of duplicate user first name"
-INPUT_FILE = "slack_input.txt"
-SECRETS_FILE = "secrets.env"
-USER_NOT_FOUND_REGEX = r"There exists no availability data for (\w+)\."
-
-
